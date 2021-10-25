04179d73a6c84c154bdde46682ff4c9b3a3e51ff
Kavya-349 <83696676+Kavya-349@users.noreply.github.com>
2021-10-25 20:34:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,6 +29,45 @@ function speak() {
     var synth = window.speechSynthesis;
     speak_data1 = "the first prediction is " + prediction_1;
     speak_data2 = "and the second prediction is " + prediction_2;
-    var utterthis = speak_data1 + speak_data2;
+    var utterthis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
     synth.speak(utterthis);
+}
+
+function check() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if(error)
+        console.error(error);
+    else {
+        console.log(results);
+
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+
+        if(results[0].label == "Best") {
+            document.getElementById("update_emoji").innerHTML = "&#128077";
+        } else if(results[0].label == "Nice") {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        } else if(results[0].label == "Left") {
+            document.getElementById("update_emoji").innerHTML = "&#128073;";
+        } else {
+            document.getElementById("update_emoji").innerHTML = "&#128072;";
+        } 
+
+        if(results[1].label == "Best") {
+            document.getElementById("update_emoji2").innerHTML = "&#128077";
+        } else if(results[1].label == "Nice") {
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        } else if(results[1].label == "Left") {
+            document.getElementById("update_emoji2").innerHTML = "&#128073;";
+        } else {
+            document.getElementById("update_emoji2").innerHTML = "&#128072;";
+        } 
+    }
 }
